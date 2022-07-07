fbf79f9c99b78ff9f8d64a8487737bc30032ea06
Barnabas27 <bnyambura032@gmail.com>
2022-07-07 09:30:36 +0300

    add component feedback list

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,10 +1,19 @@
-function FeedbackItem() {
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+
+function FeedbackItem({item}) {
+        
     return (
-        <div className="card">
-            <div className="num-display">10</div>
-            <div className="text-display">This is an example of a feedback Item</div>
-        </div>
+        <Card >
+            <div className="num-display">{ item.rating }</div>
+            <div className="text-display">{ item.text }</div>
+            
+        </Card>
     )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
