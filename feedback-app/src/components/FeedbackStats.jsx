96a01a6e6c1ce2624b94c0b642fb3d78919e253a
Barnabas27 <bnyambura032@gmail.com>
2022-07-07 10:50:15 +0300

    add component for feed back stats(avg and reviews)

---
+++ feedback-app/src/components/FeedbackStats.jsx
@@ -0,0 +1,21 @@
+import propTypes from 'prop-types'
+function FeedbackStats( {feedback} ) {
+//getting the average of reviews
+    let average = feedback.reduce((acc,cur) => {
+        return acc + cur.rating
+    },0) /feedback.length
+
+    average = average.toFixed(1).replace(/[.,]0$/, '')
+    
+    return (
+    <div className="feedback-stats">
+        <h4>{feedback.length} Reviews</h4>
+        <h4>Average Rating:{isNaN(average) ? 0 : average}</h4>
+        </div>
+  )
+}
+FeedbackStats.propTypes = {
+    feedback:propTypes.array.isRequired
+}
+
+export default FeedbackStats
