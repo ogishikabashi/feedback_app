b97ce2daa2ed0a83bbf83a38da451cecccefb042
Barnabas27 <bnyambura032@gmail.com>
2022-07-15 11:48:30 +0300

    add feedback add function

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -1,54 +1,69 @@
 import Card from "./shared/Card"
 import Button from "./shared/Button"
 import RatingSelect from "./RatingSelect"
-import {useState} from 'react'
+import { useState } from 'react'
 
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
 
     const [text, setText] = useState('')
     const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
-    
 
-    
+
+
 
     const handleTextChange = (e) => {
-        if(text === '') {
+        if (text === '') {
             setBtnDisabled(true)
             setMessage(null)
-        }else if(text !== '' && text.trim().length <= 10) {
+        } else if (text !== '' && text.trim().length <= 10) {
             setMessage('Text must be atleast 10 Characters')
             setBtnDisabled(true)
-        } else{
+        } else {
             setMessage(null)
             setBtnDisabled(false)
         }
         setText(e.target.value)
     }
-    
-  return (
-    <Card>
-        <form>
-            <h2>How would you rate your service with us?</h2>
-            <RatingSelect select={(rating) => setRating(rating)} />
-            <div className="input-group">
-                <input 
-                onChange={handleTextChange} 
-                type="text" 
-                placeholder='Write a review' 
-                value={text} />
 
-                <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+    const handleSubmit = (e) => {
+        e.preventDefault() //prevents default behavior
+        //another check
+        if(text.trim().length > 10) {
+            
+            const newFeedback = {
+                //shorthand taking up from state
+                text,
+                rating
+            }
+            handleAdd(newFeedback)
+
+            setText('') //to clear the text field
+        }
+    }
+    return (
+        <Card>
+            <form onSubmit={handleSubmit}>
+                <h2>How would you rate your service with us?</h2>
+                <RatingSelect select={(rating) => setRating(rating)} />
+                <div className="input-group">
+                    <input
+                        onChange={handleTextChange}
+                        type="text"
+                        placeholder='Write a review'
+                        value={text} />
+
+                    <Button type="submit" isDisabled={btnDisabled}>Send</Button>
 
 
-                
-            </div>
-            {message && <div className='message'>{message}</div>}
-        </form>
-    </Card>
-  )
+
+                </div>
+                {message && <div className='message'>{message}</div>}
+            </form>
+        </Card>
+    )
 }
 
 export default FeedbackForm
