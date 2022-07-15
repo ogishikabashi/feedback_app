61b3095ea12d2206abb19773b2f5d60f88b2ef0d
Barnabas27 <bnyambura032@gmail.com>
2022-07-15 11:49:48 +0300

    add modifications

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 import {FaTimes} from 'react-icons/fa'
-// import { identity } from 'lodash'
 
 function FeedbackItem({item, handleDelete}) {
     
