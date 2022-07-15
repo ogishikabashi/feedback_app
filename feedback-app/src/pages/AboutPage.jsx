e9c67e3b2493d5d1f4fea32dbf50b4888daae4f5
Barnabas27 <bnyambura032@gmail.com>
2022-07-15 12:35:29 +0300

    add an about page

---
+++ feedback-app/src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import Card from "../components/shared/Card"
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>This is react project to leave feedback for services and products</p>
+        <p>version 10.0</p>
+
+        <p>
+          <a href="/">Home</a>
+        </p>
+      </div>
+    </Card>
+    
+  )
+}
+
+export default AboutPage
