d764a32ad3b2e12be3fa1e5ad6ca2b0fb167f53f
Barnabas27 <bnyambura032@gmail.com>
2022-07-15 12:03:48 +0300

    add fadeanimation using framer motion

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,24 +1,34 @@
-import {useState} from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
-function FeedbackList({feedback, handleDelete}) {
+function FeedbackList({ feedback, handleDelete }) {
     if (!feedback || feedback.length === 0) {
         return <p>No feedback Yet</p>
     }
-  return (
-    <div className="feedback-list">
+    return (
+        <div className="feedback-list">
+            <AnimatePresence>
         {feedback.map((item) => (
-             <FeedbackItem key={item.id} 
-             item={item}
-             handleDelete={handleDelete} />
+                <motion.div
+                    key={item.id}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}>
+                    <FeedbackItem key={item.id}
+                        item={item}
+                        handleDelete={handleDelete} />
+                </motion.div>
+            
         ))}
-       
-    </div>
-  )
+            </AnimatePresence>
+
+
+        </div>
+    )
 }
 
 FeedbackList.propTypes = {
-    feedback:PropTypes.arrayOf(
+    feedback: PropTypes.arrayOf(
         PropTypes.shape({
             id: PropTypes.number.isRequired,
             text: PropTypes.string.isRequired,
