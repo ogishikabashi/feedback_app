119efc099a6421c44d9668c7ce4a96fb813fb27f
Barnabas27 <bnyambura032@gmail.com>
2022-07-16 07:38:32 +0300

    add delete funcitonality to context

--- feedback-app/src/context/FeedbackContext.js
+++ feedback-app/src/context/FeedbackContext.js
@@ -12,8 +12,14 @@ export const FeedbackProvider = ({children}) =>{
             rating: 10
         }
     ])
+    const deleteFeedback = (id) => {
+        if (window.confirm("Are you sure you want to delete?")) {
+          setFeedback(feedback.filter((item) => item.id !== id));
+        }
+      }
     return <FeedbackContext.Provider value = {{ 
         feedback,
+        deleteFeedback,
     }}>
         {children}
     </FeedbackContext.Provider>
